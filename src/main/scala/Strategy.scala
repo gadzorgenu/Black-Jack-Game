b102179225ac492786ead1c21f5aa77700e45dd2
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-11-18 16:35:57 +0000

    Added project structure

---
+++ src/main/scala/Strategy.scala
@@ -0,0 +1,4 @@
+trait Strategy {
+     def move(val score:Integer)
+
+}
---
+++ src/main/scala/Suit.scala
@@ -0,0 +1,25 @@
+object Suit extends Enumeration {
+  val Two = Value(2)
+  val Three = Value(3)
+  val Four = Value(4)
+  val Five = Value(5)
+  val Six = Value(6)
+  val Seven = Value(7)
+  val Eight = Value(8)
+  val Nine = Value(9)
+  val Ten = Value(10)
+  val King = Value(10)
+  val Queen = Value(10)
+  val Jack = Value(10)
+  val Ace = Value(11)
+}
+
+
+//public enum Suit {
+//  Two(2),Three(3),Four(4),Five(5),Six(6),Seven(7),Eight(8),Nine(9),Ten(10),King(10),Queen(10),King(10),Ace(11);
+//
+//  private int value;
+//  private Suit(int i) {
+//    value= i;
+//  }
+//}
