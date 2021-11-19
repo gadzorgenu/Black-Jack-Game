856f2f1d2a3921ebd5dfd6ddcc6e78746287038e
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-11-19 09:07:42 +0000

    Added card values

---
+++ src/main/scala/cards/CardValues.scala
@@ -0,0 +1,19 @@
+package cards
+
+class CardValues extends  Enumeration {
+
+  val Two = Value(2)
+  val Three = Value(3)
+  val Four = Value(4)
+  val Five = Value(5)
+  val Six = Value(6)
+  val Seven = Value(7)
+  val Eight = Value(8)
+  val Nine = Value(9)
+  val Ten = Value(10)
+  val King = Value(10)
+  val Queen = Value(10)
+  val Jack = Value(10)
+  val Ace = Value(11)
+
+}
--- src/main/scala/cards/Suit.scala
+++ src/main/scala/cards/Suit.scala
@@ -1,17 +1,7 @@
 package cards
 
 object Suit extends Enumeration {
-  val Two = Value(2)
-  val Three = Value(3)
-  val Four = Value(4)
-  val Five = Value(5)
-  val Six = Value(6)
-  val Seven = Value(7)
-  val Eight = Value(8)
-  val Nine = Value(9)
-  val Ten = Value(10)
-  val King = Value(10)
-  val Queen = Value(10)
-    val Jack = Value(10)
-  val Ace = Value(11)
+  type Suit = Value
+
+  val Heart, Diamond, Club , Spade = Value
 }
