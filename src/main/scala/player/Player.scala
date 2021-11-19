d7ece82e57ad92baea44a0ecaee829f3df6cc90d
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-11-19 16:50:45 +0000

    added Player impl for black jack game

--- src/main/scala/player/Player.scala
+++ src/main/scala/player/Player.scala
@@ -1,5 +1,17 @@
 package player
 
-class Player {
+import cards.Card
+import strategy.DefaultStrategy
 
+import scala.collection.mutable._
+
+class Player(val id: Integer, val strategy: DefaultStrategy = new DefaultStrategy()) {
+
+    var playerDeck: Stack[Card] = new Stack()
+
+    def initialMove(globalDeck: Stack[Card]): Unit = { playerDeck.push(globalDeck.pop); playerDeck.push(globalDeck.pop) }
+
+    def nextMove(globalDeck: Stack[Card] ): Unit = strategy.move(score, playerDeck, globalDeck)
+
+    def score() : Int =  if(!playerDeck.isEmpty) playerDeck.map(_.value._2).sum else 0
 }
