263230f562250b627efdbf4d50df0084e8137d91
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-11-19 16:51:24 +0000

    misc, for now

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,36 @@
 import cards.{Card, CardValue, Deck, Suit}
+import player.Player
 
 import scala.collection.mutable._
 
 object Main extends App {
- val sthing = new Deck()
+ var sthing = new Deck()
+ println(sthing.cards)
  println(sthing.cards.length)
- println(sthing.shuffleCards)
+ val shuffled: Stack[Card] = sthing.shuffleCards
+ println(shuffled)
+// println(somepop(sthing.cards))
+// println(sthing.cards.length)
+// println(somepop(sthing.cards))
+// println(sthing.cards.length)
+// val actualPoppingFunc = somepop(sthing.cards)
+// actualPoppingFunc()()
+// println(sthing.cards.length)
+// actualPoppingFunc()()
+// println(sthing.cards.length)
+
+ val p1 = new Player(1)
+ p1.initialMove(shuffled)
+ println(shuffled.length)
+ println(p1.playerDeck.length)
+ println(p1.score)
+ p1.nextMove(shuffled)
+ println(p1.score)
+ p1.nextMove(shuffled)
+ println(p1.score)
+ p1.nextMove(shuffled)
+ println(p1.score)
+
+
+ def somepop(s: Stack[Card]): () => () => Card =  () => () => s.pop
 }
