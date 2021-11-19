d2a7c389914f895af91cbacf67c047fd99c30309
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-11-19 12:27:23 +0000

    added blackjack card impl

---
+++ src/main/scala/cards/Card.scala
@@ -0,0 +1,5 @@
+package cards
+
+import cards.Suit.Suit
+
+case class Card(val suit:Suit, val value: (String, Int))
---
+++ src/main/scala/cards/Suit.scala
@@ -0,0 +1,7 @@
+package cards
+
+object Suit extends Enumeration {
+  type Suit = Value
+
+  val Heart, Diamond, Club , Spade = Value
+}
