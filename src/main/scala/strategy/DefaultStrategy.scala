35f530fe073c9773220034f604984911471f4afa
thenamskov <ahoryaw1@outlook.com>
2021-11-22 05:01:30 +0000

    moved methods to trait & provided right arguments to strategy methods

--- src/main/scala/strategy/DefaultStrategy.scala
+++ src/main/scala/strategy/DefaultStrategy.scala
@@ -1,29 +1,23 @@
 package strategy
 
-import cards.Card
-
 import scala.collection.mutable._
+
+import cards._
+import player._
+
 
 class DefaultStrategy extends Strategy {
 
-  def move(score: Integer, playerDeck : => Stack[Card], globalDeck : => Stack[Card], id: Int): Unit = {
+  def move(score: Integer, playerDeck : => Stack[Card], globalDeck : => Stack[Card], id: Int, players: Map[Int, Player]): Unit = {
     score match{
-      case score if(score < 17) => requestCard(globalDeck, playerDeck)
-      case score if(score >= 17 && score < 21) => stay()
-      case score if(score > 21) => forfeit(id)
+      case score if(score < 17) => requestCard(globalDeck, playerDeck, id, players)
+      case score if(score >= 17 && score < 21) => stay(id, players)
+      case score if(score > 21) => forfeit(id, players)
     }
   }
 
-  def requestCard(globalDeck: => Stack[Card], playerDeck: => Stack[Card]): Unit = {
-    playerDeck.push(globalDeck.pop())
-    println("Hit")
-  }
+}
 
-  def stay(): Unit = {
-    println("Stick")
-  }
-
-  def forfeit(id: Int): Unit = {
-    println(s"Go bust ${id}")
-  }
+object DefaultStrategy {
+  def apply = new DefaultStrategy()
 }
